a01e2f50a9c4f31a6e06f34d3480735d51d2e70a
gabobx <gabo_bx@hotmail.com>
2015-11-12 23:10:49 -0300

    Update filters.h

--- Inc/filters.h
+++ Inc/filters.h
@@ -3,12 +3,10 @@
 #include "arm_math.h"
 
 /* Exported define -----------------------------------------------------------*/
-#define LP_FS16000_50_1000_LENGTH 	64
-#define HP_FS16000_1000_50_LENGTH   64
-#define FILTER_MAX_LENGHT           64
+#define BP_LENGTH 									100
+#define FILTER_MAX_LENGHT           100
 
 /* Exported data -------------------------------------------------------------*/
-extern const q15_t lp16000_50_1000[]; 
-extern const q15_t hp16000_1000_50[];
+extern const q15_t bp[]; 
 
 /* End of file ---------------------------------------------------------------*/
